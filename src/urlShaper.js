cb457209764c2b0d58f2160024e4e6559b9bb69b
enzoborgfrantz <enzo.frantz@goeuro.com>
2017-09-08 17:21:27 +0200

    update tests and refactor code

--- src/urlShaper.js
+++ src/urlShaper.js
@@ -1,19 +1,26 @@
-const lastCharacter = (string = "") => string[string.length - 1];
+const isString = value => typeof value === 'string';
 
-const resolveSlash = (path = "") => (lastCharacter(path) === '/') ? path : `${path}/`;
+const isNonEmptyString = value => !!value && isString(value);
 
-const notNullOrUndefined = (values = []) => values
-  .every(v => ![null, undefined, 'null', 'undefined'].includes(v));
+const resolveSlash = path => path[path.length - 1] === '/' ? path : `${path}/`;
+
+const isObject = value => (!!value && !Array.isArray(value) && typeof value === 'object');
+
+const isNonEmptyObject = value => (isObject(value) && !!Object.keys(value).length);
+
+const objectToArray = (object) => Object.keys(object).map(key => [key, object[key]]);
+
+const isNullOrUndefined = value => [null, undefined, 'null', 'undefined'].includes(value);
+
+const anyNullOrUndefined = values => !values.some(isNullOrUndefined);
 
 const queryParameter = ([key, value]) => `${key}=${value}`;
 
-const objectToArray = (object = {}) => Object.keys(object).map(key => [key, object[key]]);
-
-const buildQuery = (parameters = {}) => {
+const buildQuery = (parameters) => {
   const parameterArray = objectToArray(parameters);
 
   const query = parameterArray
-    .filter(notNullOrUndefined)
+    .filter(anyNullOrUndefined)
     .map(queryParameter)
     .join('&');
 
@@ -21,7 +28,7 @@ const buildQuery = (parameters = {}) => {
 }
 
 export default (path, queryParameters) => {
-  const pathWithSlash = resolveSlash(path);
-  const query = buildQuery(queryParameters);
+  const pathWithSlash = isNonEmptyString(path) ? resolveSlash(path) : '';
+  const query = isNonEmptyObject(queryParameters) ? buildQuery(queryParameters) : '';
   return `${pathWithSlash}${query}`;
 }
--- test/urlShaper.test.js
+++ test/urlShaper.test.js
@@ -2,12 +2,15 @@ import urlShaper from '../src/urlShaper';
 
 describe('urlShaper', () => {
   const mockPath = "http://www.test.com/";
+  const mockPathWithoutSlash = `${mockPath.slice(0, -1)}`;
 
   const mockUrlParameters = {
     name: 'joe',
     surname: 'smith',
     age: 15,
     isRegistered: true,
+    favoriteQuote: '',
+    incidents: 0,
   };
 
   const mockUrlParametersWithNullAndUndefined = {
@@ -16,19 +19,36 @@ describe('urlShaper', () => {
     'null': 'world',
     undefined: 'boo',
     'undefined': 'bar',
+    hello: null,
+    world: 'null',
+    boo: undefined,
+    bar: 'undefined',
   };
+
+  const expectedUrl = `${mockPath}?name=joe&surname=smith&age=15&isRegistered=true&favoriteQuote=&incidents=0`;
+
+  test('resolves slash when not it\'s not provided in the path', () => {
+    const testUrl = urlShaper(mockPathWithoutSlash, mockUrlParameters);
+    expect(testUrl).toEqual(expectedUrl);
+  })
 
   test('builds url when valid path and valid queryParameters are provided', () => {
     const testUrl = urlShaper(mockPath, mockUrlParameters);
-    const expectedUrl = `${mockPath}?name=joe&surname=smith&age=15&isRegistered=true`;
-
     expect(testUrl).toEqual(expectedUrl);
   });
 
   test('builds url when valid path and null / undefined queryParameters are provided', () => {
     const testUrl = urlShaper(mockPath, mockUrlParametersWithNullAndUndefined);
-    const expectedUrl = `${mockPath}?name=joe&surname=smith&age=15&isRegistered=true`;
-
     expect(testUrl).toEqual(expectedUrl);
+  });
+
+  test('returns an empty string when invalid parameters are provided', () => {
+    expect(urlShaper(undefined, undefined)).toEqual('');
+    expect(urlShaper(null, null)).toEqual('');
+    expect(urlShaper('', '')).toEqual('');
+    expect(urlShaper({}, {})).toEqual('');
+    expect(urlShaper([], [])).toEqual('');
+    expect(urlShaper(0, 0)).toEqual('');
+    expect(urlShaper('', '')).toEqual('');
   });
 });
